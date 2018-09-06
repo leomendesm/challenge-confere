b94abd02c6c201dbebeb2bbbf6816390b5faa15b
Leonardo M <leo.mi.me@gmail.com>
2018-09-06 14:52:28 -0300

    fix: action name on tests

--- src/redux-flow/reducers/contents/action-creators.js
+++ src/redux-flow/reducers/contents/action-creators.js
@@ -1,13 +1,13 @@
 import axios from 'axios'
 import { APIURL, config } from './../../../utils'
-import { FETCHING_SECTION_DATA, FETCHING_SECTION_DATA_SUCCESS, FETCHING_SECTION_DATA_FAIL } from './actions'
+import { FETCHING_CONTENT_DATA, FETCHING_CONTENT_DATA_SUCCESS, FETCHING_CONTENT_DATA_FAIL } from './actions'
 
 const FetchContentData = (dispatch, id) => {
 
-    dispatch({ type: FETCHING_SECTION_DATA })
+    dispatch({ type: FETCHING_CONTENT_DATA })
         let axiosPromise = axios.get(`${APIURL}v2/sections/${id}/contents`, config)
-        .then(res => { return dispatch({ type: FETCHING_SECTION_DATA_SUCCESS, payload: res.data })})
-        .catch(err => { return dispatch({ type: FETCHING_SECTION_DATA_FAIL, payload: err.msg })})
+        .then(res => { return dispatch({ type: FETCHING_CONTENT_DATA_SUCCESS, payload: res.data })})
+        .catch(err => { return dispatch({ type: FETCHING_CONTENT_DATA_FAIL, payload: err.msg })})
         return(axiosPromise)
 }
 
--- src/redux-flow/reducers/contents/action-creators.test.js
+++ src/redux-flow/reducers/contents/action-creators.test.js
@@ -13,14 +13,14 @@ it('should dispatch two times with success', () => {
     FetchSectionData(dispatch, id)
 
     expect(mockAxios.get).toHaveBeenCalledWith(`${APIURL}v2/sections/${id}/contents`, config)
-    expect(dispatch).toHaveBeenCalledWith({"type": "FETCHING_SECTION_DATA"});
+    expect(dispatch).toHaveBeenCalledWith({"type": "FETCHING_CONTENT_DATA"});
 
     let responseObj = { data: 'test' }
     mockAxios.mockResponse(responseObj)
 
-    expect(dispatch).toHaveBeenCalledWith({"payload": "test", "type": "FETCHING_SECTION_DATA_SUCCESS"});
+    expect(dispatch).toHaveBeenCalledWith({"payload": "test", "type": "FETCHING_CONTENT_DATA_SUCCESS"});
 
-    expect(dispatch).not.toHaveBeenCalledWith({"payload": "error", "type": "FETCHING_SECTION_DATA_FAIL"});
+    expect(dispatch).not.toHaveBeenCalledWith({"payload": "error", "type": "FETCHING_CONTENT_DATA_FAIL"});
 })
 
 it('should dispatch two times with fail', () => {
@@ -30,12 +30,12 @@ it('should dispatch two times with fail', () => {
     FetchSectionData(dispatch, id)
 
     expect(mockAxios.get).toHaveBeenCalledWith(`${APIURL}v2/sections/${id}/contents`, config)
-    expect(dispatch).toHaveBeenCalledWith({"type": "FETCHING_SECTION_DATA"});
+    expect(dispatch).toHaveBeenCalledWith({"type": "FETCHING_CONTENT_DATA"});
 
     let responseObj = {msg: 'err'}
     mockAxios.mockError(responseObj)
 
-    expect(dispatch).toHaveBeenCalledWith({"payload": "err", "type": "FETCHING_SECTION_DATA_FAIL"});
+    expect(dispatch).toHaveBeenCalledWith({"payload": "err", "type": "FETCHING_CONTENT_DATA_FAIL"});
 
-    expect(dispatch).not.toHaveBeenCalledWith({"payload": "error", "type": "FETCHING_SECTION_DATA_SUCCESS"});
+    expect(dispatch).not.toHaveBeenCalledWith({"payload": "error", "type": "FETCHING_CONTENT_DATA_SUCCESS"});
 })
