d5744c08ba51ebac32a9f426d3c21d4cb311f434
Leonardo M <leo.mi.me@gmail.com>
2018-09-06 14:48:38 -0300

    feat: content reducer done

---
+++ src/redux-flow/reducers/contents/index.js
@@ -0,0 +1,41 @@
+require('dotenv').config()
+import { FETCHING_CONTENT_DATA, FETCHING_CONTENT_DATA_SUCCESS, FETCHING_CONTENT_DATA_FAIL } from './actions'
+
+export const initialState = {
+    isFetching: null,
+    data: [],
+    hasError: false,
+    errorMessage: null,
+}
+
+const content = (state = initialState, action) => {
+    switch(action.type){
+        case FETCHING_CONTENT_DATA: 
+            return Object.assign({}, state, {
+                isFetching: true,
+                data: null,
+                hasError: false,
+                errorMessage: null
+            })
+            
+        case FETCHING_CONTENT_DATA_SUCCESS:
+            return Object.assign({}, state, {
+                isFetching: false,
+                data: action.payload,
+                hasError: false,
+                errorMessage: null
+            })
+
+        case FETCHING_CONTENT_DATA_FAIL:
+            return Object.assign({}, state, {
+                isFetching: false,
+                data: null,
+                hasError: true,
+                errorMessage: action.payload
+            })
+
+        default: return state
+    }
+}
+
+export default content
