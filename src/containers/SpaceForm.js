2ed1c364f5ecd9df7833c9535bd26b635c0aea4c
Leonardo M <leo.mi.me@gmail.com>
2018-09-10 18:19:07 -0300

    feat: changed spaceform to use styled components

--- src/containers/SpaceForm.js
+++ src/containers/SpaceForm.js
@@ -1,35 +1,41 @@
 import React from 'react'
-import Select from 'react-select'
+import Select from '../components/Select'
+import Field from '../components/Field'
 import { connect } from 'react-redux'
 import FetchSpaceData, { SetSpaceOption } from '../redux-flow/reducers/spaces/action-creators'
 import FetchSectionData from '../redux-flow/reducers/sections/action-creators'
+
+
 class SpaceForm extends React.Component {
     componentWillMount(){
         this.props.FetchSpaceData()
     }
     handleChange = (selectedOption) => {
-        console.log(`Option selected:`)
-        this.props.FetchSectionData(selectedOption.id, selectedOption)
+        this.props.FetchSectionData(selectedOption.id)
         this.props.SetSpaceOption(selectedOption)
     }
     render() {
         let options = []
-        if(!this.props.spaces.isFetching && this.props.spaces.isFetching !== null){
+        
+        if(!this.props.spaces.isFetching && this.props.spaces.isFetching !== null)
             options = this.props.spaces.data.results.map(o => ({"label": o.name, "id": o.id}))
-            console.log(this.props.spaces.data)
-        }
+
         return (
-            <Select
-                value={this.props.spaces.selectedOption}
-                onChange={this.handleChange}
-                options={options}
-            />
+            <Field>
+                <label>Espaço</label>
+                <Select
+                    isDisabled={this.props.spaces.isFetching}
+                    value={this.props.spaces.selectedOption}
+                    onChange={this.handleChange}
+                    options={options}
+                />
+            </Field>
         )
     }
 }
 const mapStateToProps = state => ({
     spaces: state.spaces
-  })
+})
 
 const mapDispatchToProps = dispatch => ({
     FetchSpaceData: () => FetchSpaceData(dispatch),
@@ -38,4 +44,3 @@ const mapDispatchToProps = dispatch => ({
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(SpaceForm)
-
